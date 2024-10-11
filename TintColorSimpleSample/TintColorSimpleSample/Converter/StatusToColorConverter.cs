d349d5ce980e1f30f1baf5665534fbc4d340a59c
RiyashameedM <97871624+RiyashameedM@users.noreply.github.com>
2024-10-11 16:57:29 +0530

    Add files via upload

---
+++ TintColorSimpleSample/TintColorSimpleSample/Converter/StatusToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TintColorSimpleSample.Converter
+{
+    public class StatusToColorConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return (int)value>0 ?
+                    Colors.Yellow :
+                    Colors.Black;
+            }
+            catch (Exception ex)
+            {
+                return Colors.Black;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return null;
+        }
+
+        #endregion
+    }
+
+}
---
+++ TintColorSimpleSample/TintColorSimpleSample/MainPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TintColorSimpleSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public View View1 { get; set; }
+		public View View2 { get; set; }
+		public View View3 { get; set; }
+		public MainPage()
+		{
+			InitializeComponent();
+			this.View1 = this.Resources["View1"] as View;
+			this.View2 = this.Resources["View2"] as View;
+			this.View3 = this.Resources["View3"] as View;
+		}
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			if (sender is Button button)
+			{
+				if (button.Text == "View1")
+				{
+					((this.Content as StackLayout).Children[1] as ContentView).BindingContext = new TestSampleViewModel1();
+					((this.Content as StackLayout).Children[1] as ContentView).Content = this.View2;
+					button.Text = "View2";
+				}
+				else if (button.Text == "View2")
+				{
+					((this.Content as StackLayout).Children[1] as ContentView).BindingContext = new TestSampleViewModel2();
+					((this.Content as StackLayout).Children[1] as ContentView).Content = this.View3;
+					button.Text = "View3";
+				}
+				else if (button.Text == "View3")
+				{
+					((this.Content as StackLayout).Children[1] as ContentView).BindingContext = new TestSampleViewModel();
+					((this.Content as StackLayout).Children[1] as ContentView).Content = this.View1;
+					button.Text = "View1";
+				}
+			}
+		}
+	}
+	public class TestSampleViewModel : INotifyPropertyChanged
+	{
+		public TestSampleViewModel()
+		{
+
+		}
+
+		private int selected = 0;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public int Selected
+		{
+			get { return selected; }
+			set { selected = value; OnPropertyChanged("Selected"); }
+		}
+		protected virtual void OnPropertyChanged(string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+	public class TestSampleViewModel2 : INotifyPropertyChanged
+	{
+		public TestSampleViewModel2()
+		{
+			Selected = 1;
+		}
+
+		private int selected = 0;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public int Selected
+		{
+			get { return selected; }
+			set { selected = value; OnPropertyChanged("Selected"); }
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+	public class TestSampleViewModel1 : INotifyPropertyChanged
+	{
+		public TestSampleViewModel1()
+		{
+
+		}
+
+		private int selected = 1;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public int Selected
+		{
+			get { return selected; }
+			set { selected = value; OnPropertyChanged("Selected"); }
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+	}
+}
